061ffd7a359b37a66a806bd8643f3a6e72fa2cc2
Sergios Sampai <s.sampai@pmmretail.com>
2023-10-19 13:34:15 +0300

    Initial Commit

---
+++ src/main/java/com/example/casestudyfullstack/CaseStudyFullStackApplication.java
@@ -0,0 +1,13 @@
+package com.example.casestudyfullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaseStudyFullStackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CaseStudyFullStackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/casestudyfullstack/CaseStudyFullStackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.casestudyfullstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaseStudyFullStackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
